b676131f1c69caa14172a285836c7ad086fadd60
psvensson <psvensson@gmail.com>
2015-12-04 14:59:17 +0100

    fixed flow widget creation problem with multiple wires and added blescan widget

--- libs/nrfe.js
+++ libs/nrfe.js
@@ -6,6 +6,14 @@ var nrfe = function(target)
 	console.log = hyper.log;
 }
 
+var dump = function(o)
+{
+	for(var p in o)
+	{
+		console.log(p+' -> '+o[p]);
+	}
+}
+
 //------------------------------------------------------------------------------- Table o' widgets
 
 var inst_table =
@@ -18,21 +26,11 @@ var inst_table =
 		node.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent";
 		node.innerHTML = def.name;
 
-		var events = ['click'];
-
-		events.forEach(function(handler)
-		{
-			//console.log('adding event handler '+handler+' for widget '+def.type);
-			node.addEventListener(handler, function(e)
-			{
-				def.out({payload:e})
-			});
-		});
-
 		def.in = function(msg)
 		{
 			console.log('ohayoo - button got input message');
 			console.dir(msg);
+			node.innerHTML = msg.payload;
 		};
 
 		return node;
@@ -61,17 +59,6 @@ var inst_table =
 		var node = document.createElement('input');
 		node.className = "mdl-textfield__input";
 
-		var events = ['click','change','keypress','keyup','keydown', 'focus','blur'];
-
-		events.forEach(function(handler)
-		{
-			//console.log('adding event handler '+handler+' for widget '+def.type);
-			node.addEventListener(handler, function(e)
-			{
-				def.out({payload:e})
-			});
-		});
-
 		return node;
 	},
 	'page': function(def, parent)
@@ -79,6 +66,7 @@ var inst_table =
 		var node = document.createElement('div');
 		node.className="mdl-grid";
 		node.style.background = def.background;
+		node.style.height = "100%";
 		node.style.flexDirection = def.direction || 'col';
 		return node;
 	},
@@ -119,10 +107,14 @@ var inst_table =
 		//node.innerHTML = "fooz";
 		def.in = function(msg)
 		{
-			//console.log('template got message');
-			//console.dir(JSON.stringify(msg));
-			//console.log('template is '+def.template);
-			node.innerHTML = Mustache.render(def.template, msg)
+			if(msg && msg.payload)
+			{
+				console.log('template got message');
+				//console.dir(JSON.stringify(msg.payload));
+				dump(msg.payload);
+				//console.log('template is '+def.template);
+				node.innerHTML = Mustache.render(def.template, msg.payload)
+			}
 		};
 		console.log('---- creating template '+def.name);
 		console.dir(node);
@@ -132,7 +124,7 @@ var inst_table =
 	{
 		def.in = function(msg)
 		{
-			var func = new Function('msg', def.func);
+			var func = new Function('msg', def.func).bind(def);
 			var rv = func(msg);
 			def.out(rv);
 		};
@@ -150,13 +142,39 @@ var inst_table =
 			}
 			else
 			{
+				console.log('starting scanning')
 				def.scanning = true;
 				evothings.ble.startScan(function(r)
 				{
-					msg.payload = r
+					msg.payload = r;
 					def.out(msg);
 				});
 			}
+		};
+
+	},
+	'bleservices': function(def, parent)
+	{
+		def.in = function(msg)
+		{
+			console.log('ble-services called for address '+msg.payload.address);
+			evothings.ble.connect(msg.payload.address, function(r)
+			{
+				var deviceHandle = r.deviceHandle;
+				console.log('connect '+r.deviceHandle+' state '+r.state);
+				if (r.state == 2) // connected
+				{
+					console.log('connected, requesting services...');
+					evothings.ble.readAllServiceData(deviceHandle, function(services)
+					{
+						def.out({payload: services});
+					});
+				}
+			}, function(errorCode)
+			{
+				console.log('connect error: ' + errorCode);
+				def.out({payload: {error:'connect error: ' + errorCode}});
+			});
 		};
 
 	},
@@ -164,36 +182,60 @@ var inst_table =
 	{
 		var node = document.createElement('div');
 		node.className = "mdl-cell mdl-cell--4-col mdl-cell--stretch";
+		node.style.marginLeft = "0";
 		//node.style = "container: 'flex', flexDirection: "+def.direction;
 		console.log(JSON.stringify(def));
 
-		tdef = def.picklist.split(',');
+		var tdef = def.picklist.split(',');
+		var idproperty = def.idproperty || 'name'
 
 		var table = document.createElement('table');
-		table.className = "mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp";
+		table.className = "mdl-data-table mdl-js-data-table  mdl-shadow--2dp";
+
 		var thead = document.createElement('thead');
 		table.appendChild(thead);
 		var thtr = document.createElement('tr');
+
 		thead.appendChild(thtr);
 		tdef.forEach(function(colname)
 		{
 			var th = document.createElement('th');
-			th.innerHTML = colname;
+			th.style.background = "#ddd";
+			th.style.fontWeight = "bold";
+			th.className="mdl-data-table__cell--non-numeric";
+			th.innerHTML = '<strong>'+colname+'</strong>';
 			thtr.appendChild(th);
 		});
 		var tbody = document.createElement('tbody');
 		table.appendChild(tbody);
 
+		var seenitems = []
+
 		def.in = function(msg)
 		{
-			var tr = document.createElement('tr');
-			tbody.appendChild(tr);
-			tdef.forEach(function(colval)
+			var item = msg.payload;
+			//console.log("---------------------- picklist population");
+			console.log(JSON.stringify(item));
+			if(item && item[idproperty] && !seenitems[item[idproperty]])
 			{
-				var td = document.createElement('td');
-				td.innerHTML = msg.payload[colval];
-				tr.appendChild(td);
-			});
+				console.log('adding new row for item '+item[idproperty]);
+				seenitems[item[idproperty]] = item;
+				var tr = document.createElement('tr');
+				tbody.appendChild(tr);
+				tdef.forEach(function(p)
+				{
+					var td = document.createElement('td');
+					td.className="mdl-data-table__cell--non-numeric";
+					td.innerHTML = item[p];
+					tr.appendChild(td);
+				});
+				tr.addEventListener('click', function(e)
+				{
+					var p = {payload:item};
+					console.log('sending item '+JSON.stringify(p));
+					def.out(p)
+				});
+			}
 		};
 
 		//-----------------------
@@ -211,14 +253,14 @@ nrfe.prototype.render = function(fedef)
 	var page = undefined;
 	if(fedef)
 	{
-		console.log('rendering fe def '+fedef);
-		console.dir(fedef);
 		fedef.forEach(function(wdef)
 		{
-			if(wdef.type === 'page')
-			{
-				page = wdef;
-			}
+
+				if(wdef.type === 'page')
+				{
+					page = wdef;
+				}
+
 			this.widgets[wdef.id] = wdef
 		}.bind(this));
 		this.renderWidget(page, this.target);
@@ -228,17 +270,18 @@ nrfe.prototype.render = function(fedef)
 nrfe.prototype.renderWidget = function(widget_def, parentNode)
 {
 	//console.log('NRFE parsing widget '+widget_def.type);
-	if(widget_def)
+	if(widget_def   )
 	{
 		var node = widget_def.node || this.instantiateWidget(widget_def, parentNode);
 		widget_def.node = node;
-		if(parentNode && widget_def.node && !widget_def.node.parentNode)
+		if(parentNode && widget_def.node)
 		{
 			//console.log('* * appending '+widget_def.type+' under '+parentNode);
 			parentNode.appendChild(node);
 		}
-		if(widget_def.wires)
+		if(widget_def.wires && !widget_def.wired)
 		{
+			widget_def.wired = true
 			widget_def.wires.forEach(function(widget_def_child)
 			{
 				widget_def_child.forEach(function(childid)
@@ -246,6 +289,20 @@ nrfe.prototype.renderWidget = function(widget_def, parentNode)
 					this.renderWidget(this.widgets[childid], node);
 				}.bind(this));
 			}.bind(this));
+		}
+		if(node && widget_def.events && !widget_def.evented)
+		{
+			widget_def.evented = true;
+			var events = widget_def.events.split(',');
+			//var events = ['click','change','keypress','keyup','keydown', 'focus','blur'];
+			events.forEach(function(handler)
+			{
+				//console.log('adding event handler '+handler+' for widget '+def.type);
+				node.addEventListener(handler, function(e)
+				{
+					widget_def.out({payload:e})
+				});
+			});
 		}
 	}
 };
@@ -273,7 +330,7 @@ nrfe.prototype.instantiateWidget = function(widget_def, parentNode)
 			{
 				warr.forEach(function(wid)
 				{
-					//console.log('sending message to widget '+wid);
+					//console.log('sending message to widget '+wid+' type '+widget_def.type);
 					var w = this.widgets[wid];
 					//console.log(JSON.stringify(w));
 					w.in(msg);
